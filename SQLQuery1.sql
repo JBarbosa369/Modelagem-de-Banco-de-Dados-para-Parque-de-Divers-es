a57ab527b8bb0e3149e6be5682000ebe269c1d3d
JBarbosa369 <jeovana.nascimento@aluno.senai.br>
2025-08-22 10:37:34 -0500

    alteração feita hoje 22.08.2025

--- SQLQuery1.sql
+++ SQLQuery1.sql
@@ -1,86 +1,42 @@
-CREATE DATABASE parque
-USE parque
-
-CREATE TABLE Clientes(
-id INT PRIMARY KEY IDENTITY (1,1),
-nome VARCHAR (255),
-Idade VARCHAR (255)
-)
-
-INSERT INTO Clientes(nome,Idade)
-VALUES('Jeovana','20')
-
-INSERT INTO Clientes(nome,Idade)
-VALUES('Nayara','14')
-
-INSERT INTO Clientes(nome,Idade)
-VALUES('Jeova','22')
-
-INSERT INTO Clientes(nome,Idade)
-VALUES('Alzenir','32')
-
-INSERT INTO Clientes(nome,Idade)
-VALUES('Maxwell','22')
-
-SELECT * FROM Clientes
+CREATE DATABASE parque_diversao
+USE parque_diversao
 
 
-CREATE TABLE Brinquedos(
-id INT PRIMARY KEY IDENTITY (1,1),
-nome VARCHAR (255),
-altura_minima VARCHAR (255),
-preço VARCHAR (255)
-)
+CREATE TABLE Clientes (
+id_cliente INT PRIMARY KEY IDENTITY(1,1),
+nome VARCHAR(100),
+FOREIGN KEY (id_cliente) REFERENCES Clientes(id_cliente)
 
-INSERT INTO Brinquedos(nome,altura_minima,preço)
-VALUES('Montanha-Russa',' 140 cm','R$ 15,00'),
-('Roda-Gigante','100 cm',' R$ 10,00'),
-('Carrinho de Bate-Bate','110 cm','R$ 12,00'),
-('Barco Viking',' 130 cm ',' R$ 14,00'),
-('Castelo do Terror','120 cm','R$ 13,00')
-
-SELECT*FROM Brinquedos
+);
 
 
-CREATE TABLE Comidas(
-id INT PRIMARY KEY IDENTITY (1,1),
-nome VARCHAR (255),
-preço VARCHAR (255)
-)
-
-INSERT INTO Comidas(nome,preço)
-VALUES('Pipoca ','R$ 6,00'),
-('Algodão-Doce','R$ 5,00'),
-('Cachorro-Quente','R$ 8,00'),
-('Hambúrguer','R$ 12,00'),
-('Refrigerante',' R$ 5,00'),
-('Sorvete','R$ 7,00')
-
-SELECT*FROM Comidas
-
-CREATE TABLE Funcionarios(
-id INT PRIMARY KEY IDENTITY (1,1),
-nome VARCHAR (255),
-salario VARCHAR (255)
-)
-
-INSERT INTO Funcionarios(nome,salario)
-VALUES('Ana Paula','R$ 2.200,00'),
-('Marcos Vinícius ','R$ 2.500,00'),
-('Fernanda Lima','R$ 2.300,00'),
-('João Pedro',' R$ 2.100,00'),
-('Luana Martins','R$ 2.400,00')
+CREATE TABLE Brinquedos (
+id_brinquedo INT PRIMARY KEY IDENTITY(1,1),
+nome VARCHAR(100),
+FOREIGN KEY (id_brinquedo) REFERENCES Brinquedos(id_brinquedo)
+);
 
 
-SELECT*FROM Funcionarios
+CREATE TABLE Comidas (
+id_comida INT PRIMARY KEY IDENTITY(1,1),
+nome VARCHAR(100),
+preco DECIMAL(10,2),
+FOREIGN KEY (id_comida) REFERENCES Comidas(id_comida)
 
+);
 
+CREATE TABLE Uso_Brinquedos (
+id_uso INT PRIMARY KEY IDENTITY(1,1),
+id_cliente INT,
+id_brinquedo INT,
+FOREIGN KEY (id_cliente) REFERENCES Clientes(id_cliente),
+FOREIGN KEY (id_brinquedo) REFERENCES Brinquedos(id_brinquedo)
+);
 
-DROP TABLE 
-
-
-
-
-
-
-
+CREATE TABLE Consumo_Comidas (
+id_consumo INT PRIMARY KEY IDENTITY(1,1),
+id_cliente INT,
+id_comida INT,
+FOREIGN KEY (id_cliente) REFERENCES Clientes(id_cliente),
+FOREIGN KEY (id_comida) REFERENCES Comidas(id_comida)
+);
